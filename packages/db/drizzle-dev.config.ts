e4df96507ccf709baf8b01113918d0870456b063
darjss <darjsavid@gmail.com>
2025-04-14 14:31:27 +0800

    seperated db as a package

---
+++ packages/db/drizzle-dev.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from "drizzle-kit";
+
+export default {
+  schema: "./src/schema.ts",
+  out: "./drizzle/migrations",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: "./dev.db",
+  },
+} satisfies Config;
