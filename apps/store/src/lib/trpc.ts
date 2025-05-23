7a2175c17bea2b10facce20e4c6218dd26d0bb81
darjss <darjsavid@gmail.com>
2025-05-23 12:37:50 +0800

    fix conflict

--- apps/store/src/lib/trpc.ts
+++ apps/store/src/lib/trpc.ts
@@ -71,5 +71,3 @@ export const trpc = createTRPCOptionsProxy<AppRouter>({
   client: api,
   queryClient,
 });
-
-
---
+++ apps/store/src/lib/types.d.ts
@@ -0,0 +1,20 @@
+import type { CustomerSelectType } from "@vit/db/schema";
+export interface ProductCard {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+}
+export interface Cart {
+    productId: number;
+    quantity: number;
+    name: string;
+    price: number;
+    image: string;
+}
+export interface Session {
+    id: string;
+    user: CustomerSelectType;
+    expiresAt: Date;
+}
+//# sourceMappingURL=types.d.ts.map
